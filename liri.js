039dd5b3008b7c1afe2d84a9655c26e3b48706e5
Polinet <uchpolinet@gmail.com>
2019-08-18 16:22:29 +1000

    Music Search and New File
    Created a function to search for songs and a new javascript file to store client id and password as Spotify api requires an authentication token

--- liri.js
+++ liri.js
@@ -1,4 +1,18 @@
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var dotenv = require("dotenv");
-// var moment =
+
+var Spotify = require("node-spotify-api");
+
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+
+Spotify.search({ type: "track", query: "All the Small Things" })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
