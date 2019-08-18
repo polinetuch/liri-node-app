a0141ec53834a9396afd5bb5ea0f19b3420a686d
Polinet <uchpolinet@gmail.com>
2019-08-18 17:54:43 +1000

    Created more files
    created files: .env, .gitignore, random.txt, keys.js and reinstall npm

--- liri.js
+++ liri.js
@@ -1,24 +1,20 @@
 // Files required
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var request = require("request");
-var dotenv = require("dotenv").config();
 
 // input variable that takes in argument 2 for search function
 var argvMusicSearch = process.argv[2];
 
-// function spotifyThis(musicSearch) {
-var spotify = new Spotify({
-  id: "d0b25bc85d8242b9a93e2db403efedaa",
-  secret: "5e54edb622bb4dc6bcd11bb3d9b9a085"
-});
+var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: "track", query: "Beautiful People" }, function(
-  error,
-  data
-) {
+spotify.search({ type: "track", query: "One Time" }, function(error, data) {
   if (error) {
     return console.log(error);
   } else {
-    console.log(data.tracks);
+    for (var i = 0; i < data.tracks.items.length && i < 5; i++) {
+      console.log(data.tracks.items[i]);
+    }
   }
 });
