34e1e7e6b54b88c2faca7e495d253e71087bada4
Polinet <uchpolinet@gmail.com>
2019-08-18 17:15:46 +1000

    Received response from spotify api
    removed keys.js file and store spotify id and secret in the same file by assigning in a variable. Responses displayed in the console log

--- liri.js
+++ liri.js
@@ -1,18 +1,24 @@
+// Files required
 var Spotify = require("node-spotify-api");
 var request = require("request");
-var dotenv = require("dotenv");
+var dotenv = require("dotenv").config();
 
-var Spotify = require("node-spotify-api");
+// input variable that takes in argument 2 for search function
+var argvMusicSearch = process.argv[2];
 
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
+// function spotifyThis(musicSearch) {
+var spotify = new Spotify({
+  id: "d0b25bc85d8242b9a93e2db403efedaa",
+  secret: "5e54edb622bb4dc6bcd11bb3d9b9a085"
+});
 
-Spotify.search({ type: "track", query: "All the Small Things" })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+spotify.search({ type: "track", query: "Beautiful People" }, function(
+  error,
+  data
+) {
+  if (error) {
+    return console.log(error);
+  } else {
+    console.log(data.tracks);
+  }
+});
